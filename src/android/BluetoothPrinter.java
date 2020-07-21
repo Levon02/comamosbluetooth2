0174b472bdd4ae5f6d99d35c30cc7d7b008f114d
Levon02 <68303232+Levon02@users.noreply.github.com>
2020-07-21 03:03:41 +0200

    Update BluetoothPrinter.java

--- src/android/BluetoothPrinter.java
+++ src/android/BluetoothPrinter.java
@@ -107,7 +107,8 @@ public class BluetoothPrinter extends CordovaPlugin {
         } else if (action.equals("printText")) {
             try {
                 String msg = args.getString(0);
-                printText(callbackContext, msg);
+                String msg_2 = args.getString(1);
+                printText(callbackContext, msg_2);
             } catch (IOException e) {
                 Log.e(LOG_TAG, e.getMessage());
                 e.printStackTrace();
@@ -432,7 +433,7 @@ public class BluetoothPrinter extends CordovaPlugin {
 
     }
 
-    boolean printText(CallbackContext callbackContext, String msg) throws IOException {
+    boolean printText(CallbackContext callbackContext, String msg, String msg_2) throws IOException {
         try {
             //Chinese Zeichen deaktivieren
             //mmOutputStream.write(0x1C);
